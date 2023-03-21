f4761893fc3a9502af14dffe27fde395d46c6748
iharoun <islem.haroun@my-digital-school.org>
2023-03-21 16:29:01 +0100

    app file added

--- src/Routes/app.ts
+++ src/Routes/app.ts
@@ -1,3 +1,26 @@
-import test from "node:test";
+import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import pool from './data_base';
 
-test
+const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
+dotenv.config();
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hi');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
